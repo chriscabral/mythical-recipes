c37817948914bc455172afbc9e8141ef29ed7c40
Chris Cabral <chris.cabral@newbrandanalytics.com>
2013-10-30 07:13:46 -0400

    trying different forms of include

--- cookbooks/mythical/metadata.rb
+++ cookbooks/mythical/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vccabral@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures mythical'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.4'
 
 depends 'apt'
 depends 'gitorious'
