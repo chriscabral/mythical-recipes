c0b84cbfdb3782444d6f78232bb7d732a8cc8da8
Chris Cabral <chris.cabral@newbrandanalytics.com>
2013-10-30 06:47:17 -0400

    initial startup script

---
+++ cookbooks/mythical/metadata.rb
@@ -0,0 +1,9 @@
+name             'mythical'
+maintainer       'mythical.io'
+maintainer_email 'vccabral@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures mythical'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
+
+depends 'gitorious'
---
+++ cookbooks/mythical/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: mythical
+# Recipe:: default
+#
+# Copyright 2013, mythical.io
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'gitorious'
